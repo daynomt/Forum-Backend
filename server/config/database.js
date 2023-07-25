92918cbd74312cfa87dbf07c3e4db343166085fc
daynomt <temesgnmogo@gmail.com>
2023-07-25 12:02:36 -0400

    database deploy

--- server/config/database.js
+++ server/config/database.js
@@ -1,15 +1,16 @@
 const mysql = require("mysql2");
-const pool = mysql.createPool({
-  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.MYSQL_DB,
-  password: process.env.DB_PASS,
-  connectionLimit: 10,
-});
-pool.getConnection(function (err, connection) {
-  console.log("Database connected");
-});
+// const pool = mysql.createPool({
+//   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   database: process.env.MYSQL_DB,
+//   password: process.env.DB_PASS,
+//   connectionLimit: 10,
+// });
+const pool = mysql.createConnection(process.env.DATABASE_URL);
+// const pool = pool.getConnection(function (err, connection) {
+//   console.log("Database connected");
+// });
 let registration = `CREATE TABLE if not exists registration(
 user_id int auto_increment,
 user_name varchar(255) not null,
@@ -22,9 +23,7 @@ user_profile_id int auto_increment,
 user_id int not null,
 first_name varchar(255) not null,
 last_name varchar(255) not null,
-PRIMARY KEY (user_profile_id),
-FOREIGN KEY(user_id) REFERENCES registration(user_id)
-)`;
+PRIMARY KEY (user_profile_id))`;
 
 let question = `CREATE TABLE if not exists question(
 question_id int auto_increment,
@@ -35,8 +34,8 @@ tags varchar(255),
 post_id varchar(255) not null,
 user_id int not null,
 PRIMARY KEY (question_id),
-UNIQUE KEY(post_id),
-FOREIGN KEY(user_id)REFERENCES registration(user_id))`;
+UNIQUE KEY(post_id)
+)`;
 
 let answer = `CREATE TABLE if not exists answer(
 answer_id int auto_increment,
@@ -44,9 +43,7 @@ answer varchar(255) not null,
 answer_code_block varchar(255),
 user_id int not null,
 question_id int not null,
-PRIMARY KEY (answer_id),
-FOREIGN KEY(user_id)REFERENCES registration(user_id),
-FOREIGN KEY(question_id)REFERENCES question(question_id)
+PRIMARY KEY (answer_id)
 )`;
 pool.query(registration, (err, results) => {
   if (err) throw err;
